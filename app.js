75281c7d4d3968e6ae9d8daf6c72c0b06cbaac20
Tamana543 <tamanafarzami33@gmail.com>
2025-10-01 16:16:04 +0430

    file updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const path = require('path')
-
+const mainRoute = require('./routes/main')
 
 
 app.use(bodyParser.urlencoded({extended : false}))
@@ -13,6 +13,8 @@ app.set('view engine','ejs')
 app.use((req,res,next)=>{
      res.status(404).render('404',{pageTitle : 'Page not Fond'})
 })
+
+app.use(mainRoute)
 app.listen(5420,()=>{
 
      console.log("From node js");
